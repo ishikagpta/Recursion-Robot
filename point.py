335b83b262293e19a130edf166ed943b4c463b3d
Ishika Gupta <ishika.ig.gupta@gmail.com>
2021-09-07 18:07:36 -0700

    Robot code

---
+++ point.py
@@ -0,0 +1,13 @@
+# Point class is made to represent a coordinate point
+class Point:
+
+
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
